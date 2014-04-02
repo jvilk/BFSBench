72b653a1dbc7ed57c8b31d4292a9f51580f35ddd
John Vilk <jvilk@cs.umass.edu>
2014-04-01 21:31:25 -0400

    Adding replayer frontend.

---
+++ tools/event_replayer.ts
@@ -0,0 +1,11 @@
+/// <reference path="../lib/DefinitelyTyped/node/node.d.ts" />
+import event_logger = require('./event_logger');
+var argv = process.argv;
+
+if (argv.length !== 4) {
+  console.log("Usage: node " + argv[1] + " [name of record] [data directory]");
+  process.exit();
+}
+
+process.chdir(argv[3]);
+var replayer = new event_logger.EventReplay(argv[2]);
